09cbb8d7690cab034fe0c9c4226a17f8eb304766
patrick <patrickchukwudifu716@gmail.com>
2021-07-24 08:41:15 -1200

    make it better

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,  login, logout
 from django.contrib import messages, auth
